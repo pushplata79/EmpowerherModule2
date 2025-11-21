c5fbdaa3c0847fa90b76e24f89f7c6922deb9a68
poshiplatta Bagati <85026844+pushplata79@users.noreply.github.com>
2025-11-21 20:41:01 +0530

    promises async await

--- Day14/notes/js1.js
+++ Day14/notes/js1.js
@@ -1,1 +1,51 @@
 
+// 1.breakfast
+//2.clean kitchen
+//3.take out trash
+
+function breakfast(){
+  
+      return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+          const breakkfst=false;
+          if(breakkfst){
+            resolve("done with breakfast");
+        
+          }
+          else{
+            reject("didn't");
+          }
+          
+            
+      }, 1500);
+      
+      });
+}
+function cleankitchen(){
+  return new Promise((resolve,reject)=>{
+    
+    setTimeout(()=>{
+      const cleankit=false;
+      if
+      resolve("clean the kitchen");
+  
+      }, 3500);
+  });
+  
+}
+  function takeouttrash(){
+    return new Promise((resolve,reject)=>{
+
+  setTimeout(()=>{resolve("take out dustbin");
+  
+      }, 500);
+});
+
+}
+
+
+breakfast().then(value=>{console.log(value); return cleankitchen()})
+           .then(value=>{console.log(value); return takeouttrash()})
+           .then(value=>{console.log(value); console.log("yoo finished")})
+           .catch(error=>console.error(error)); 
+           
